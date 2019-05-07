05d0be571905cd1ceef4339373134d4d16cf0641
dimamohov <49777959+dimamohov@users.noreply.github.com>
2019-05-07 22:15:40 +0300

    Add files via upload

---
+++ homeDZ1/calc.c
@@ -0,0 +1,12 @@
+// Домашнее задание №1. Калькулятор
+
+#include <stdio.h>
+
+void main()
+{
+	int a = 10;
+	int b = 25;
+	printf("a+b=%d \n",a+b);
+	printf("a-b=%d \n",a-b);
+	printf("a*b=%d \n",a*b);
+}
